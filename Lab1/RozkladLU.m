62310b7bbbdf4b73f9819d8b8062b7fce5563a34
241721 <39262993+241721@users.noreply.github.com>
2020-03-26 12:08:57 +0100

    Input system updated
    Dialog box added

--- Lab1/RozkladLU.m
+++ Lab1/RozkladLU.m
@@ -1,7 +1,19 @@
 clear all;
 clc;
-A  = [2 1 1 -1;1 1 -1 1; 1 1 1 1; -1 2 -1 1];
-y = [3;4;10;4];
+prompt = {'Separator rzêdów (;):'};
+dlgtitle = 'Wprowadz macierz wspolczynnikow A';
+answer = inputdlg(prompt,dlgtitle, [1 75]);
+answer = strcat('[',answer,']');
+A = str2num(answer{1}) %Macierz wspó³czynników
+prompt = {'Separator rzêdów (;):'};
+dlgtitle = 'Wprowadz wektor danych b';
+answer = inputdlg(prompt,dlgtitle, [1 75]);
+answer = strcat('[',answer,']');
+y = str2num(answer{1}); %Wektor danych
+
+A  = [2 1 1 -1;1 1 -1 1; 1 1 1 1; -1 2 -1 1]; %Macierz wspólczynnikow
+y = [3;4;10;4]; %Wektor danych
+
 [L,U]=lu(A);
 x = L*U\y;
 disp("Rozwi¹zanie uk³adu: ");
