f47e0cff7efe293f24ca08baa27fb0ad90df6b98
241721 <39262993+241721@users.noreply.github.com>
2020-04-21 16:30:22 +0200

    Metoda potęgowa
    Metoda potęgowa obliczanie, wersja 1

---
+++ Lab3/Metoda potęgowa/pow.m
@@ -0,0 +1,19 @@
+A=[9 9 8; 9 9 9; 9 9 9];
+x=[1;1;1];
+format short
+m=1000;
+tol = 0.01;
+i=1;
+for i=1:m
+    y = A*x;
+    x = y/y(1);
+    if abs(min(x))<tol
+        break;
+    end
+end
+x=x/x(end);
+disp(x)
+disp(i)
+disp(max(y))
+
+
