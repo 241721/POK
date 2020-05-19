f26d4ffe84e9896b2393d319efe98c460cf2b4e8
241721 <39262993+241721@users.noreply.github.com>
2020-05-20 01:11:41 +0200

    MonteCarlo update

--- Lab5/MonteCarlo.m
+++ Lab5/MonteCarlo.m
@@ -3,11 +3,12 @@ counter = 0;    %zmienna do zliczania ile probek znajduje sie pod funkcja
 for i=1:n
     rPoint = [rand * 3 + 2, rand*30];   %losowy punkt z przedziału y (0, 30) oraz x = 2 5
     
-    if rPoint(2) <= rPoint(1)^2+3 %jesli wartosc punktu mniejsza od wartosci funkcji
+    if rPoint(2) <= rPoint(1)^2+3 %jesli wartosc punktu mniejsza od wartosci funkcji w danym punkcie
         counter = counter + 1;      %zwieksz licznik probek o 1
     end
     
 end
+
 
 calka = (counter/n)*(3*30);
 disp("Całka wynosi: " + calka);
